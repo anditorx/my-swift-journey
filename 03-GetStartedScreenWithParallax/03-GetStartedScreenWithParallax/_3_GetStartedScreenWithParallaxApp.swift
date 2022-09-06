97e1e7d58f0d442fedb29d570804fc2dc466b1a5
Andi Rustianto <anditodev46@gmail.com>
2022-09-06 17:03:17 +0700

    init project 03-GetStartedScreenWithParallax

---
+++ 03-GetStartedScreenWithParallax/03-GetStartedScreenWithParallax/_3_GetStartedScreenWithParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_GetStartedScreenWithParallaxApp.swift
+//  03-GetStartedScreenWithParallax
+//
+//  Created by Armstrong on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct _3_GetStartedScreenWithParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
